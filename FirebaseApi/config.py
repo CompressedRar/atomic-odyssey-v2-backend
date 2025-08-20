8e67fd917b9f19e5ff06b57140838b21860c2b2f
CompressedRar <qwertythanzip1103@gmail.com>
2025-08-20 23:35:29 +0800

    234

--- FirebaseApi/config.py
+++ FirebaseApi/config.py
@@ -1,11 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-cred = credentials.Certificate("FirebaseApi/atomic-odyssey-firebase-adminsdk-fbsvc-6ac5414c03.json")
-firebase_admin.initialize_app(cred)
+
+firebase_key_path = os.getenv("FIREBASE_KEY")
+cred = credentials.Certificate(firebase_key_path)
+firebase_admin.initialize_app(cred, {
+    'apiKey': "AIzaSyCCSvYFzj3bEDaSer6hp1NZwa9vCaeUDAY",
+  'authDomain': "atomic-odyssey.firebaseapp.com",
+  'projectId': "atomic-odyssey",
+  'storageBucket': "atomic-odyssey.firebasestorage.app",
+  'messagingSenderId': "969855183693",
+  'appId': "1:969855183693:web:90fa6ad424d094fa25a504"
+})
 
 db = firestore.client()
-profile_storage = storage.bucket("profile-bucket")
+profile_storage = storage.bucket()
 
 
 UserDB = db.collection("users")
